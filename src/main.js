ababf8c75126fe1481e25bda9cd3e32b410c247a
xcl268 <131644441+xcl268@users.noreply.github.com>
2023-12-20 20:41:45 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
+import echarts from "@/echarts.js";
 
 const app = createApp(App)
+
+app.config.globalProperties.$echarts = echarts;
+app.provide('$echarts', echarts);
 
 app.use(router)
 
