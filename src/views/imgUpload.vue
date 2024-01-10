385ad117662cfcdaee40ea88801e140861528973
Raymond <raymond4ever@163.com>
2024-01-10 18:07:01 +0800

    更新homepage

---
+++ src/views/imgUpload.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+//
+const activeIndex = ref(1);
+function handleSelect() {
+
+}
+
+</script>
+
+<template>
+  <main>
+    <div id="topBar">
+      <el-menu
+          :default-active="activeIndex"
+          mode="horizontal"
+          @select="handleSelect"
+      >
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>Workspace</template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
+          <el-menu-item index="2-3">item three</el-menu-item>
+          <el-sub-menu index="2-4">
+            <template #title>item four</template>
+            <el-menu-item index="2-4-1">item one</el-menu-item>
+            <el-menu-item index="2-4-2">item two</el-menu-item>
+            <el-menu-item index="2-4-3">item three</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4">Orders</el-menu-item>
+      </el-menu>
+    </div>
+    <div id="content">
+
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
